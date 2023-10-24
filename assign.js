0e99f2f24b101d53e82ef9eb9501f07e302fb688
Natsunsn <Natsunan270@gmail.com>
2023-10-24 15:37:22 +0700

    first commit

---
+++ assign.js
@@ -0,0 +1,18 @@
+//Assignment Opeartor
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+console.log(`a: ${a}`)
+
+//1.Assignment[=]
+consolee/log(`a: ${a}`)
+
+//2. addtition Assignment[+=]
+console.log(`b:${b+=5}`); //b=b+5
+
+//3.addition Assignment[+=]
+console.log(`c: ${c += 10}`); //c=c-10
+
+//4.Multipiy Assignment[*=]
+console.log(`d:${d *=15}`); //d=d *15
---
+++ compare.js
@@ -0,0 +1,15 @@
+//Compare operator
+const a = 2;
+const b = '2';
+//1.Equal Operator
+console.log(2 == 2); //true
+console.log(2 == '2'); //true
+//2.Not Equal Operator
+console.log(2 !=2); //false
+console.log('hello' !='Hello'); //true
+//3.String equal Operator
+console.log(2 === 2);//true
+console.log(2 === '2'); //false
+//4.String not equal Operator
+console.log(2!=='2');//true
+console.log(2!== 2);//false
